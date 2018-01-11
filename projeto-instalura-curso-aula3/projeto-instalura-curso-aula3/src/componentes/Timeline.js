b25478cd4ef2edd57556cd856b270e0f8594b830
Tiago <web2.wplay@gmail.com>
2018-01-11 17:52:19 -0200

    aula 3 em andamento

--- projeto-instalura-curso-aula3/projeto-instalura-curso-aula3/src/componentes/Timeline.js
+++ projeto-instalura-curso-aula3/projeto-instalura-curso-aula3/src/componentes/Timeline.js
@@ -9,10 +9,10 @@ export default class Timeline extends Component {
     }
 
     componentDidMount(){
-        fetch(`https://instalura-api.herokuapp.com/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
+      fetch(`http://localhost:8080/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
        .then(response => response.json())
        .then(fotos => {
-         this.setState({fotos: fotos});
+         this.setState({fotos:fotos});
        });
     }
 
