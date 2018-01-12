777328fe3794bdb2578c96747f5ab31384472145
Tiago <web2.wplay@gmail.com>
2018-01-12 17:28:39 -0200

    aula 4 em andamento

--- instalura/src/componentes/Timeline.js
+++ instalura/src/componentes/Timeline.js
@@ -12,7 +12,7 @@ export default class Timeline extends Component {
         fetch(`https://instalura-api.herokuapp.com/api/fotos?X-AUTH-TOKEN=${localStorage.getItem('auth-token')}`)
        .then(response => response.json())
        .then(fotos => {
-         this.setState({fotos: fotos});
+         this.setState({fotos:fotos});
        });
     }
 
